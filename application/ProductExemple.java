ec413ffaef501640628b51e419ad3a6aadba9dd5
pgrigolli <pedrogrigolli30@gmail.com>
2024-02-02 14:57:11 -0300

    Refactor ProductExemple and Product classes

--- application/ProductExemple.java
+++ application/ProductExemple.java
@@ -10,27 +10,29 @@ public class ProductExemple {
         
         Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
-        Product p1 = new Product();
         
         System.out.println("Entre com um produto: ");
         System.out.print("Nome: ");
-        p1.name = sc.nextLine();
+        String name = sc.nextLine();
         System.out.print("Preço: ");
-        p1.price = sc.nextInt();
-        System.out.print("Quantidade em estoque: ");
-        p1.quantity = sc.nextInt();
+        double price = sc.nextDouble();
+
+        Product p1 = new Product(name, price);
 
         System.out.println(p1);
-
+        
         System.out.println();
         System.out.print("Entre com a quantidade de produtos a serem adicionados ao estoque: ");
-        int quantity = sc.nextInt();
-        p1.AddProducts(quantity);
+        int quantity_add = sc.nextInt();
+        p1.AddProducts(quantity_add);
         System.out.println("Produtos adicionados com sucesso. Quantidade atual: " + p1.quantity);
+        System.out.println(p1);
+        
         System.out.print("Entre com a quantidade de produtos a serem removidos do estoque: ");
-        quantity = sc.nextInt();
-        p1.RemoveProducts(quantity);
+        quantity_add = sc.nextInt();
+        p1.RemoveProducts(quantity_add);
         System.out.println("Produtos removidos com sucesso. Quantidade atual: " + p1.quantity);
+        System.out.println(p1);
         
         
 
--- entities/Product.java
+++ entities/Product.java
@@ -6,28 +6,58 @@ public class Product {
     public double price;
     public int quantity;
 
+
+    public Product(){
+    }
+
+    public Product(String name, double price, int quantity){
+        this.name = name;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Product(String name, double price){
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public double getPrice(){
+        return price;
+    }
+
+    public void setPrice(double price){
+        this.price = price;
+    }
+
+    public int getQuantity(){
+        return quantity;
+    }
+
     public double TotalValueInStock(){
         return price * quantity;
     };
     
     public void AddProducts(int quantity){
-
         this.quantity += quantity;
-
     };
 
     public void RemoveProducts(int quantity){
-
         this.quantity -= quantity;
-
     };
 
     public String toString(){
-        return name + ", $" + price + ", " + quantity + " units, Total: $ " + TotalValueInStock();
-
-
-
+        return name + ", $" + String.format("%.2f", price) + ", " + quantity + " units, Total: $ " + String.format("%.2f", TotalValueInStock());
     }
+
+
 
 
 }
