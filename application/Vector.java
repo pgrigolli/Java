6d8e650fee58514626be18630488b4b3897ba866
pgrigolli <pedrogrigolli30@gmail.com>
2024-02-14 13:19:15 -0300

    Add Data class and remove unnecessary imports in Vector class

--- application/Vector.java
+++ application/Vector.java
@@ -4,8 +4,6 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
 import java.util.Scanner;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.stream.Collectors;
 // import entities.Product;
 
