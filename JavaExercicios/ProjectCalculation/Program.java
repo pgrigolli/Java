99845bb09de2be8e9e080a5a7fcdb5d3fac1fc46
Pedro Grigolli <pedrogrigolli30@gmail.com>
2024-03-12 19:10:18 -0300

    asdasdasd

---
+++ JavaExercicios/ProjectCalculation/Program.java
@@ -0,0 +1,19 @@
+package ProjectCalculation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Program {
+
+    public static void main(String[] args){
+
+        List<Product> list = new ArrayList<>();
+
+        String path = " ";
+
+    }
+
+
+
+}
+
