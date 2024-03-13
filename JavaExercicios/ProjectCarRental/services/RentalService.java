7e2d3e298bf0d29286287ae25a25da9e8e7b1d1e
Pedro Grigolli <pedrogrigolli30@gmail.com>
2024-03-13 16:39:29 -0300

    Remove unnecessary code in RentalService.java

--- JavaExercicios/ProjectCarRental/services/RentalService.java
+++ JavaExercicios/ProjectCarRental/services/RentalService.java
@@ -8,10 +8,7 @@ public class RentalService {
     private double pricePerhour;
     private double pricePerDay;
 
-<<<<<<< HEAD
     private TaxService taxService;
-=======
->>>>>>> 99845bb09de2be8e9e080a5a7fcdb5d3fac1fc46
 
     public RentalService(){
         
